321e828d87d04008cd2b308ddf9fb6fc98e9f2d3
steven-kamanga <kamangasteven00@gmail.com>
2024-03-13 14:50:00 +0200

    one print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-
-for i in range(ord('z'), ord('a') - 1, -1):
-    if i % 2 == 0:
-        print(chr(i).lower(), end='')
-    else:
-        print(chr(i).upper(), end='')
+print(''.join('{}{}'.format(chr(i).lower(), chr(i - 1).upper()) for i in range(ord('z'), ord('a'), -2)))
