78bd6d7594473828ab8aaa917b4cfb76e411cfcc
steven-kamanga <kamangasteven00@gmail.com>
2024-03-23 08:43:18 +0200

    complete data structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        return (sum([x * y for x, y in my_list]) / sum([y for x, y in my_list]))
