06973f8f0c4cd79c1fd4d44bbf1be4b19ed50944
steven kamange <kamangasteven00@gmail.com>
2024-05-29 16:09:33 +0200

    privilages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- List all privilages of MySql users user_0d_1and
--- and user_0d_2 on local server (localhost)
+-- script that lists all privileges MySQL Users
+-- user_0d_1 and user_0d_2 on your server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
